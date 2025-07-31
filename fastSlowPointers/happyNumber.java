9bbd3fd690034fbc34bff9eeb69c1f64f64f8ef6
AccelR8 <55751739+Axelord100@users.noreply.github.com>
2025-07-31 16:51:45 -0600

    Update and rename happyNumberBrute.java to happyNumber.java

--- fastSlowPointers/happyNumber.java
+++ fastSlowPointers/happyNumber.java
@@ -38,3 +38,23 @@ public boolean happy(List<Integer> list, int n) {
 
     return happy(list, sum);
 }
+
+
+// Reducing Runtime 
+public boolean isHappy(int n) {
+    if(n == 1 || n == 7)
+        return true;
+
+    if(n < 10)
+        return false;
+
+    int sum = 0;
+
+    while(n > 0) {
+        int r = n % 10;
+        sum += r * r;
+        n = n / 10;
+    }
+
+    return isHappy(sum);
+}
