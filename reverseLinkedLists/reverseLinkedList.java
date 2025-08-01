297b6695746f4a1b3d10a7d8fcc7e5eba2cb2dd0
AccelR8 <55751739+Axelord100@users.noreply.github.com>
2025-08-01 09:03:47 -0600

    Create reverseLinkedList.java

---
+++ reverseLinkedLists/reverseLinkedList.java
@@ -0,0 +1,22 @@
+/*
+  Time: O(n)
+  Space: O(1)
+*/
+
+public ListNode reverseList(ListNode head) {
+    if(head == null)
+        return head;
+    
+    ListNode current = head;
+    ListNode prev = null;
+    ListNode next = null;
+
+    while(current != null) {
+        next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+    }
+
+    return prev;
+}
