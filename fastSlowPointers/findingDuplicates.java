d045b97ba2740db4daaea73a31f1d4c2ad7a750e
AccelR8 <55751739+Axelord100@users.noreply.github.com>
2025-07-31 18:16:42 -0600

    Create findingDuplicates.java

---
+++ fastSlowPointers/findingDuplicates.java
@@ -0,0 +1,29 @@
+/* First try brute force
+
+  Time: O(n)
+  Space: O(n)
+*/
+public int findDuplicate(int[] nums) {
+    Map<Integer, Integer> map = new HashMap<>();
+    int left = 0;
+    int right = nums.length - 1;
+
+    while(left <= right) {
+        if(left != right) {
+            map.put(nums[left], map.getOrDefault(nums[left], 0) + 1);
+            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+        }
+        else
+            map.put(nums[left], map.getOrDefault(nums[left], 0) + 1);
+
+        if(map.get(nums[left]) > 1)
+            return nums[left];
+        
+        if(map.get(nums[right]) > 1)
+            return nums[right];
+
+        left++;
+        right--;
+    }
+    return 0;
+}
