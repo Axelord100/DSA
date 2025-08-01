72919f302856965a1b0fc0c05d50cc54af7f3230
AccelR8 <55751739+Axelord100@users.noreply.github.com>
2025-07-31 18:58:18 -0600

    Update findingDuplicates.java

--- fastSlowPointers/findingDuplicates.java
+++ fastSlowPointers/findingDuplicates.java
@@ -27,3 +27,18 @@ public int findDuplicate(int[] nums) {
     }
     return 0;
 }
+
+/*
+  Time: O(nlogn)
+  Spcace: O(logn)
+*/
+public int findDuplicate(int[] nums) {
+    Arrays.sort(nums);
+
+    for(int i = 1; i < nums.length; i++) {
+        if(nums[i] == nums[i - 1])
+            return nums[i];
+    }
+    
+    return 0;
+}
